b41a18aea9690fb84481f55c8b0e44617017f1cd
Cleyton Gama <cleytongama@gmail.com>
2018-09-20 08:20:24 -0300

    primeira query graphql

--- dist/app.js
+++ dist/app.js
@@ -1,2 +1,17 @@
-console.log("Hello Word");
-let a = 'abbb';
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const express = require("express");
+const graphqlHTTP = require("express-graphql");
+const schema_1 = require("./graphql/schema");
+class App {
+    constructor() {
+        this.express = express();
+        this.middleware();
+    }
+    middleware() {
+        this.express.use('/graphql', graphqlHTTP({
+            schema: schema_1.default
+        }));
+    }
+}
+exports.default = new App().express;
---
+++ dist/graphql/schema.js
@@ -0,0 +1,32 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const graphql_tools_1 = require("graphql-tools");
+const users = [
+    {
+        id: '1',
+        name: 'Jon',
+        email: 'jon@email.com'
+    },
+    {
+        id: '2',
+        name: 'Jin',
+        email: 'jin@email.com'
+    }
+];
+const typeDefs = `
+    type User {
+        id: ID!
+        name: String!
+        email: String!
+    }
+
+    type Query {
+        allUsers:[User!]!
+    }
+`;
+const resolvers = {
+    Query: {
+        allUsers: () => [users]
+    }
+};
+exports.default = graphql_tools_1.makeExecutableSchema({ typeDefs, resolvers });
